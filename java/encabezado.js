5123ee2f1946641d4503bf2c3c9c0f56969d2989
CarolinaDume <dumecarolina@gmail.com~>
2022-10-20 17:24:26 -0300

    primer commit

---
+++ java/encabezado.js
@@ -0,0 +1,17 @@
+encabezado=`
+<div class="logo">
+<img src="./Imagenes/89ff4f4b61ee4337b62c945ad4abf9e0-removebg-preview.png" alt="">
+</div>
+</br>
+<nav id="nav">
+    <a class="link" href="./index.html">Inicio</a>
+    <a class="link" href="./nosotras.html">Nosotras</a>
+    <a class="link" href="./niño.html">Niño/Niña</a>
+    <a class="link" href="./Datos.html">Datos diarios</a>
+    <a class="link" href="./Mapas.html">Mapas</a>
+    <a class="link" href="./equipos2.html">Equipos</a>
+    <a class="link" href="./formulario.html">Suscripción</a>
+    <a class="link" href="#contactos">Contacto</a>
+</nav>
+`
+document.write(encabezado)
