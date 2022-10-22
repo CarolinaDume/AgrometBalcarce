e06eb6f0892c1901ea82f06784c06f5a5be893dc
Nuria <53791646+Nuria1982@users.noreply.github.com>
2022-10-21 21:14:31 -0300

    Add files via upload

--- java/footer.js
+++ java/footer.js
@@ -1,14 +1,14 @@
 footer=`
 <div class="footer">
-    <div id="logo col-s-4">
+    <div class="logo col-4 col-s-6">
         <img src="./Imagenes/logo.png" alt="">
     </div>
-    <div id="copy">
+    <div class="copy col-4 col-s-6">
         <h5>Copyright 2022-2025. Todos los derechos reservados</h5>
         <h6>Desarrollado por Climatología 4.0</h6>
         <a class="link" href="#nav">Volver</a>           
     </div>
-    <div id="contactos">
+    <div class="contactos col-4 col-s-12">
         <h5>Podes escribirnos en <a href="mailto:agromet@agromet.gob.ar" target="_blank">agromet@agromet.com.ar</a></h5>
         <h5>o buscanos en las redes sociales</h5>
             <a href="https://www.instagram.com/agromet_inta.balcarce/#" target="_blank"><img src="./Imagenes/instagram.png" alt="instagram" style="width:24px;height:24px;"></a>
